8ec4dcc094d2ea269da11d7263ac0efcd75344f2
Ajinkya Bapat <ajinkyabapat1@gmail.com>
2020-01-01 11:09:47 +0530

    project setup + Shooping cart design done

---
+++ src/app/components/store-front/store-front.component.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { Product } from "../../models/product.model";
+import { ShoppingCart } from "../../models/shopping-cart.model";
+import { ProductsDataService } from "../../services/products.service";
+import { ShoppingCartService } from "../../services/shopping-cart.service";
+import { Observable } from "rxjs";
+import { Observer } from "rxjs";
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-store-front",
+  styleUrls: ["./store-front.component.scss"],
+  templateUrl: "./store-front.component.html"
+})
+export class StoreFrontComponent implements OnInit {
+  public products: Observable<Product[]>;
+
+  public constructor(private productsService: ProductsDataService,
+                     private shoppingCartService: ShoppingCartService) {
+  }
+
+  public addProductToCart(product: Product): void {
+    console.log(product);
+    this.shoppingCartService.addItem(product, 1);
+  }
+
+  public removeProductFromCart(product: Product): void {
+    this.shoppingCartService.addItem(product, -1);
+  }
+
+  public productInCart(product: Product): boolean {
+    return Observable.create((obs: Observer<boolean>) => {
+      const sub = this.shoppingCartService
+                      .get()
+                      .subscribe((cart) => {
+                        obs.next(cart.items.some((i) => i.productId === product.id));
+                        obs.complete();
+                      });
+      sub.unsubscribe();
+    });
+  }
+
+  public ngOnInit(): void {
+    this.products = this.productsService.all();
+  }
+}
